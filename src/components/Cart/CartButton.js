406ee9c88b3078d591438dd4389e3de28c408663
Jahan <34219176+JahanUoL@users.noreply.github.com>
2021-12-27 21:19:35 +0000

    finished code - action creator thunk, http requests

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,21 +1,20 @@
-import classes from './CartButton.module.css';
-import { uiActions } from '../store/ui-slice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { uiActions } from '../../store/ui-slice';
+import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
-
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
-  const toggleCartHandler = (e) => {
-    dispatch(uiActions.toggleCart());
-  }
-
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
 
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import store from './store/index';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './components/store/index';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
